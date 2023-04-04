61d86e4ec2503bdd397974d3b7c2d9dc4bc3433b
Michael Brown <brownieboy@gmail.com>
2023-04-04 19:56:58 +1000

    chore: vite app installed, but caching still

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,14 +1,12 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import { Header } from "header";
+import { Footer } from "footer";
 
 export default function Index() {
-    return (
-        <>
-            <Header/>
-            <div>
-                Content!
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div>Content!</div>
+      <Footer />
+    </>
+  );
 }
