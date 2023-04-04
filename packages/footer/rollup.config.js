1c17f48f97bd5a5458fafaf6907f0e27f90c43ef
Michael Brown <brownieboy@gmail.com>
2023-04-04 20:24:27 +1000

    chore: footer exports module

--- packages/footer/rollup.config.js
+++ packages/footer/rollup.config.js
@@ -1,17 +1,18 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
 
 export default {
-    input: ["src/index.tsx"],
-    output: [
-        {
-            dir: "dist",
-            entryFileNames: "[name].js",
-            format: "cjs",
-            exports: "named"
-        }
-    ],
-    plugins: [
-        typescript(),
-    ],
-    external: ["react"]
+  input: ["src/index.tsx"],
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      exports: "auto",
+    },
+    {
+      file: "dist/index.mjs",
+      format: "esm",
+    },
+  ],
+  plugins: [typescript()],
+  external: ["react"],
 };
